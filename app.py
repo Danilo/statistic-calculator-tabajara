cbcb062238ce106426ebc825493dcf8330c1b2e8
danilo <danilo.maldito@gmail.com>
2015-04-15 19:37:04 -0300

    Changed name indice to Efi.

--- app.py
+++ app.py
@@ -2,16 +2,6 @@ import os
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
-# 18,26,21,24,26,18,19,21,18,21,24,26,28,26,21,18,19,21,21,20,21,22,18,19,21,22,18,19,21,19
-# 1	 18	 6	20.00	6   20.00
-# 2	 19	 5	16.67  11   36.67
-# 3	 20	 1	 3.33  12   40.00
-# 4	 21	 9	30.00  21   70.00
-# 5	 22	 2	 6.67  23   76.67
-# 6	 24	 2	 6.67  25   83.33
-# 7	 26	 4	13.33  29   96.67
-# 8	 28	 1	 3.33  30  100.00
-
 # 111,90,121,105,122,61,128,112,128,93,108,138,88,110,112,112,97,128,102,125,87,119,104,116,96,114,107,113,80,113,123,95,115,70,115,101,114,127,92,103,78,118,100,115,116,98,119,72,125,109,79,139,75,109,123,124,108,125,116,83,94,106,117,82,122,99,124,84,91,130
 # 01  61|--71   02   2.86%  02    2.86%
 # 02  71|--81   05   7.14%  07   10.00%
@@ -30,13 +20,21 @@ def index():
 def data():
 	
 	if request.method == 'POST':
+		print len(request.form['dados'])
+		
+		if len(request.form['dados']) == 0:
+			return """
+				<h1>Statistic Calculator Tabajara v1.0 - Flask Edition</h1>
+				<br/>
+				<h1>Data error!!!</h1>
+				"""
 		
 		if request.form['form_id'] == 'discreta':
-			dados_brutos = data_to_rol(request.form['dados'])
+			dados_brutos = data_to_rol(request.form['dados'].encode('UTF8').split())
 			discreta = Discreta()
-			discreta.indice = 8
 			discreta.insert_xi(dados_brutos)
 			discreta.insert_fi(dados_brutos)
+			discreta.Efi = len(discreta.xi)
 			discreta.fr = [20.00, 16.67, 3.33, 30.00, 6.67, 6.67, 13.33, 3.33]
 			discreta.F = [6, 11, 12, 21, 23, 25, 29, 30]
 			discreta.Fr = [20.00, 36.67, 40.00, 70.00, 76.67, 83.33, 96.67, 100.00]
@@ -52,19 +50,23 @@ def data():
 			continua.Fr = [2.86, 10.00, 18.57, 32.86, 50.00, 75.71, 97.14, 100.00]
 			return render_template('continua.html', statistic=continua)
 		else:
-			return render_template("""
+			return """
 				<h1>Statistic Calculator Tabajara v1.0 - Flask Edition</h1>
 				<br/>
 				<h1>Request error!!!</h1>
-				""")
+				"""
 
 def data_to_rol(my_list):
-	my_list.sort()
-	return my_list
+	rol = []
+	
+	for x in my_list:
+		rol.append(int(x))
+	rol.sort()
+	return rol
 
 
 class Discreta(object):
-	indice = 0
+	Efi = 0
 	xi = []
 	fi = []
 	fr = []
@@ -72,7 +74,7 @@ class Discreta(object):
 	Fr = []
 	
 	def __init__(self):
-		self.indice = 0
+		self.Efi = 0
 		self.xi = []
 		self.fi = []
 		self.fr = []
@@ -120,4 +122,5 @@ class Continua(object):
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=port)
+	app.run(debug=True)
+	# app.run(host='0.0.0.0', port=port)
