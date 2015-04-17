aed3f86d860c4526cf1c63572d8f7abe707b3fd9
danilo <danilo.maldito@gmail.com>
2015-04-17 17:04:35 -0300

    Discreta working fine.

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def data():
 			discreta.insert_Exi_fi(discreta.media)
 			discreta.insert_moda(discreta.xi, discreta.fi)
 			discreta.lines = len(discreta.xi)
-			return render_template('discreta.html', statistic=discreta)
+			return render_template('discreta.html', statistic=discreta, rol=dados_brutos)
 		elif request.form['form_id'] == 'continua':
 			dados_brutos = data_to_rol([111, 90, 121, 105, 122, 61, 128, 112, 128, 93, 108, 138, 88, 110, 112, 112, 97, 128, 102, 125, 87, 119, 104, 116, 96, 114, 107, 113, 80, 113, 123, 95, 115, 70, 115, 101, 114, 127, 92, 103, 78, 118, 100, 115, 116, 98, 119, 72, 125, 109, 79, 139, 75, 109, 123, 124, 108, 125, 116, 83, 94, 106, 117, 82, 122, 99, 124, 84, 91, 130])
 			continua = Continua()
